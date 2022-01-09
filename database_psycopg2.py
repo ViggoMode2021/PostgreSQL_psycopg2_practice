aebe2c3c748604a4c9f06a1e4e2fa33b33aa7809
ViggoMode210 <86791607+ViggoMode2021@users.noreply.github.com>
2022-01-08 20:59:34 -0500

    Update database_psycopg2.py
    ADDED GUI WITH TKINTER AND GUI STILL NEEDS SOME WORK.

--- database_psycopg2.py
+++ database_psycopg2.py
@@ -1,4 +1,9 @@
 import psycopg2
+from tkinter import *
+
+root = Tk()
+root.title('database')
+root.geometry("500x500")
 
 hostname = 'localhost'
 database = 'user_students'
@@ -8,54 +13,84 @@ port_id = 5432
 conn = None
 cur = None
 
-try:
+def update():
+    try:
+        conn = psycopg2.connect(
+            host = hostname,
+            dbname = database,
+            user = username,
+            password = pwd,
+            port = port_id)
+
+        cur = conn.cursor()
+
+        cur.execute('DROP TABLE IF EXISTS user_students')
+
+        create_script = '''CREATE TABLE IF NOT EXISTS user_students (
+            name       varchar(40) NOT NULL,
+            salary     int,
+            occupation    varchar(30)) '''
+
+        cur.execute(create_script)
+
+        insert_script = "INSERT INTO user_students (name, salary, occupation) VALUES (%s, %s, %s)"
+
+        insert_values = [(name_editor.get(), salary_editor.get(), occupation_editor.get())]
+
+        for record in insert_values:
+            cur.execute(insert_script, record)
+
+        cur.execute('SELECT * FROM USER_STUDENTS')
+        for record in cur.fetchall():
+            print(record)
+
+        conn.commit()
+    except Exception as error:
+        print(error)
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
+
+'''#create submit function
+def submit():
     conn = psycopg2.connect(
-        host = hostname,
-        dbname = database,
-        user = username,
-        password = pwd,
-        port = port_id)
+            host = hostname,
+            dbname = database,
+            user = username,
+            password = pwd,
+            port = port_id)
 
-    cur = conn.cursor()
+    c = conn.cursor()
 
-    cur.execute('DROP TABLE IF EXISTS employee')
+    c.execute("INSERT INTO user_students VALUES (:name, :salary, :occupation)",
+            {
+             'name': name.get(),
+             'salary': salary.get(),
+             'occupation': occupation.get(),
+            })
 
-    create_script = '''CREATE TABLE IF NOT EXISTS employee (
-        id         int PRIMARY KEY,
-        name       varchar(40) NOT NULL,
-        salary     int,
-        dept_id    varchar(30)) '''
+    #insert into table
+'''
 
-    cur.execute(create_script)
+name_editor = Entry(root, width = 30)
+name_editor.grid(row=0, column =1, padx=20, pady=(10, 0))
 
-    insert_script = "INSERT INTO employee (id, name, salary, dept_id) VALUES (%s, %s, %s, %s)"
-    name = input('Input name: ')
-    salary = int(input('Input salary: '))
-    occupation = input('Input occupation: ')
-    insert_values = [(1, name, salary, occupation)]
+salary_editor = Entry(root, width = 30)
+salary_editor.grid(row=1, column =1, padx=20)
 
-    for record in insert_values:
-        cur.execute(insert_script, record)
+occupation_editor = Entry(root, width = 30)
+occupation_editor.grid(row=2, column =1, padx=20)
 
-    insert_script = "INSERT INTO employee (id, name, salary, dept_id) VALUES (%s, %s, %s, %s)"
-    print('Now input the next one.')
-    name = input('Input name: ')
-    salary = int(input('Input salary: '))
-    occupation = input('Input occupation: ')
-    insert_values = [(2, name, salary, occupation)]
+name_label = Label(root, text= "Name:")
+name_label.grid(row=0, column = 0, pady=(10, 0))
+salary_label = Label(root, text= "Salary:")
+salary_label.grid(row=1, column = 0)
+occupation_label = Label(root, text= "Occupation:")
+occupation_label.grid(row=2, column = 0)
 
-    for record in insert_values:
-        cur.execute(insert_script, record)
+submit_button = Button(root, text="Add record to database", command=update)
+submit_button.grid(row=6, column =0, columnspan=2, pady=10, padx=10, ipadx=137)
 
-    cur.execute('SELECT * FROM EMPLOYEE')
-    for record in cur.fetchall():
-        print(record)
-
-    conn.commit()
-except Exception as error:
-    print(error)
-finally:
-    if cur is not None:
-        cur.close()
-    if conn is not None:
-        conn.close()
+root.mainloop()
